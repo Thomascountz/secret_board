d2628b4ed2afda4939201feeb030e476b3bb8a0d
Thomas Countz <thomascountz@gmail.com>
2017-02-26 23:14:36 +0000

    updated login and logout routes to point to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get '/login', to: 'sessions#new'
 
-  get 'sessions/create'
+  post '/login', to: 'sessions#create'
 
-  get 'sessions/destroy'
+  get '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
