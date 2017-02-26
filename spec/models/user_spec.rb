fff3b55580e49e6891b1da5aa8027faea4ee53fe
Thomas Countz <thomascountz@gmail.com>
2017-02-26 22:10:58 +0000

    added two user validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) do
+    User.create(name: 'Thomas Countz', email: 'thomascountz@example.com')
+  end
+  
+  it 'is valid' do
+    expect(user).to be_valid
+  end
+  
+  it 'is invalid without a name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+  
+  it 'is invalid without an email' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+  
 end
