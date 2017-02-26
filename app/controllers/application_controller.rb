5c56c434da6e9c5746daaad7da5eef04bed65a6f
Thomas Countz <thomascountz@gmail.com>
2017-02-26 23:01:24 +0000

    added root placeholder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def root
+    render html: '<h1>This is where the secret posts will be listed!</h1>'.html_safe
+  end
 end
