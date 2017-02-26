b34b126820f300f9018fc785283baf31aa1013e0
Thomas Countz <thomascountz@gmail.com>
2017-02-26 23:51:17 +0000

    delete unneeded specs

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders a user login form" do
+    render
+
+    assert_select "form[action=?][method=?]", login_path, "post" do
+
+      assert_select "input#session_[name=?]", "email"
+
+      assert_select "input#session_[name=?]", "password"
+    end
+  end
 end
