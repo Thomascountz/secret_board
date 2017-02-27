7d21820e370b9ba1523875d0e663464937394a2b
Thomas Countz <thomascountz@gmail.com>
2017-02-27 00:09:52 +0000

    handled login error where user doesn't exist in db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user
+      #login
+    else
+      flash.now[:danger] = "Invalid email and/or password."
+      render 'new'
+    end
   end
 
   def destroy
