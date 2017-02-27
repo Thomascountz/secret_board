ebbb24a4cad60b482a642d95f3f71508e8fad4c0
Thomas Countz <thomascountz@gmail.com>
2017-02-27 01:24:45 +0000

    added spec for invalid user login

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
-describe 'the person view', type: :feature do
-  describe 'user login' do
-    
+describe 'login', type: :feature do
+  
+  let(:user) { User.create(name: 'Thomas Countz',
+                           email: 'thomascountz@gmail.com',
+                           password: 'password',
+                           password_confirmation: 'password') }
+                           
+  describe "with a user email that doesn't exist" do
+  
+    it 'rerender the form with an alert message' do
+    visit login_path
+    page.fill_in('session_email', with: 'nonexistent@example.com')
+    page.fill_in('session_password', with: 'password')
+    page.click_button('Login')
+    expect(current_path).to eq(login_path)
+    expect('.alert').to be_present
+    end
     
     
   end
+  
 end
