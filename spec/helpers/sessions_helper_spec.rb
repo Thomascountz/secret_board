321d0dd87d20a6c6f1fd48660154f8f8a63829e5
Thomas Countz <thomascountz@gmail.com>
2017-02-28 01:49:50 +0000

    complete session helper specs

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -24,4 +24,27 @@ RSpec.describe SessionsHelper, type: :helper do
       expect(session[:user_id]).to eq(user.id)
     end
   end
+  
+  describe "logged_in?" do
+    it 'returns false if there is no user logged in' do
+      expect(logged_in?).to be false
+    end
+    
+    it 'returns true if there is a user logged in' do
+      log_in(user)
+      expect(logged_in?).to be true
+    end
+  end
+  
+  describe "current_user" do
+    it 'returns the signed in user' do
+      log_in(user)
+      expect(current_user).to eq(user)
+    end
+    
+    it 'returns nil if no user is signed in' do
+      expect(current_user).to be nil
+    end
+  end
+  
 end
