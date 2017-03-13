b1fe7dd146fac884ebd050cd86e5276a9281d2b2
Thomas Countz <thomascountz@gmail.com>
2017-03-13 15:21:44 +0000

    added user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,19 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  
+  
+  let(:valid_user) { { name:                  'Thomas Countz', 
+                       email:                 'thomascountz@example.com',
+                       password:              'password',
+                       password_confirmation: 'password' } }
+
+  let(:invalid_user) { { name:                  'a', 
+                         email:                 'thomascountz@invalid',
+                         password:              'password',
+                         password_confirmation: 'different_password' } }
 
   describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+    it "assigns a new user as @user" do
+      get :new, params: {}, session: nil
+      expect(assigns(:user)).to be_a_new(User)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    context "with valid params" do
+      
+      xit "creates a new user" do
+        expect {
+          post :create, params: { user: valid_user }, session: nil
+        }.to change(User, :count).by(1)
+      end
+      
+      xit "assigns a newly created User as @user" do
+        
+      end
+      xit "redirects to root url" do
+        
+      end
+    end
+    
+    context "with invalid params" do
+      xit "assigns a new created but unsaved user as @user" do
+        
+      end
+      xit "rerenders the new template" do
+        
+      end
     end
   end
-
 end
