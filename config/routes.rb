cc389337984f58a7c7df0543bdea181bce994379
Thomas Countz <thomascountz@gmail.com>
2017-03-13 14:59:34 +0000

    added routes for new and create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :new, :create, :destroy]
-
+  resources :users, only: [:new, :create]
+  
   get '/login', to: 'sessions#new'
 
   post '/login', to: 'sessions#create'
