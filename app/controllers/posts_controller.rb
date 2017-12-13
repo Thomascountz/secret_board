974296cee9a3523acd61699f6fa5c66555033d75
Thomas Countz <thomascountz@gmail.com>
2017-12-13 08:06:02 -0500

    resolve N+1 query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :get_user, only: [:new, :create]
   
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   def new
